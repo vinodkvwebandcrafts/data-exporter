472f596d5d044e57b6d2f5c4fd7fe01169f374af
vinod <vinod.kv@webandcrafts.com>
2026-04-27 14:46:05 +0530

    feat: register plugin::data-exporter.export RBAC action on bootstrap

--- server/src/bootstrap.ts
+++ server/src/bootstrap.ts
@@ -1,7 +1,15 @@
 import type { Core } from "@strapi/strapi";
 
-const bootstrap = ({ strapi }: { strapi: Core.Strapi }) => {
-  // bootstrap phase
+const bootstrap = async ({ strapi }: { strapi: Core.Strapi }) => {
+  await strapi.admin.services.permission.actionProvider.registerMany([
+    {
+      uid: "export",
+      pluginName: "data-exporter",
+      displayName: "Export collections",
+      section: "contentTypes",
+      subjects: ["plugin::content-manager.contentType"],
+    },
+  ]);
 };
 
 export default bootstrap;
